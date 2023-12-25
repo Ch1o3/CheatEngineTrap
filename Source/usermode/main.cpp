eccba002d4a29a4a05920c620cc9a263525cffbe
Ch1o3 <dlogns11@gmail.com>
2023-12-26 03:10:22 +0900

    유저모드 안티치트

--- Source/usermode/main.cpp
+++ Source/usermode/main.cpp
@@ -5,37 +5,36 @@
 #include <iostream>
 using namespace std;
 
-bool GetProcessModule(DWORD dwPID, char* sProcessName);
-
 bool FindProcess(char* ProcessName)
 {
+    TCHAR Message[50];
     HANDLE  hProcessSnap = NULL;
-    BOOL bRet = FALSE;
     PROCESSENTRY32 pe32 = { 0 };
     hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hProcessSnap == (HANDLE)-1)
-        return false;
     pe32.dwSize = sizeof(PROCESSENTRY32);
     //프로세스가 메모리상에 있으면 첫번째 프로세스를 얻는다
     if (Process32First(hProcessSnap, &pe32))
     {
-        BOOL bCurrent = FALSE;
-        MODULEENTRY32 me32 = { 0 };
         do
         {
-            if (GetProcessModule(pe32.th32ProcessID, ProcessName) != NULL)
+            if (!strcmp(pe32.szExeFile, ProcessName))
             {
-                HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
-                if (hProcess)
+                int pid = pe32.th32ProcessID;
+                HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+                printf("%d\n", hProcess);
+                if (hProcess != NULL)
                 {
-                    if(TerminateProcess(hProcess, 0))
+                    if (TerminateProcess(hProcess, 0))
                     {
-                        unsigned long nCode; //프로세스 종료 상태 
-                        GetExitCodeProcess(hProcess, &nCode);
+                        sprintf(Message, "%s 프로세스를 종료합니다.", pe32.szExeFile);
+                        MessageBox(NULL, Message, "Error", MB_OK);
+                        CloseHandle(hProcess);
+                        CloseHandle(hProcessSnap);
                     }
-                    
-                    CloseHandle(hProcess);
-                    return true;
+                    else
+                    {
+                        // Handle TerminateProcess failure if needed
+                    }
                 }
             }
         } while (Process32Next(hProcessSnap, &pe32)); //다음 프로세스의 정보를 구하여 있으면 루프를 돈다.
@@ -44,45 +43,8 @@ bool FindProcess(char* ProcessName)
     return false;
 }
 
-
-bool GetProcessModule(DWORD dwPID, char* sProcessName)
-{
-    HANDLE hModuleSnap = NULL;
-    MODULEENTRY32 me32 = { 0 };
-    TCHAR process[50];
-
-    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
-    if (hModuleSnap == (HANDLE)-1)
-        return (FALSE);
-    me32.dwSize = sizeof(MODULEENTRY32);
-    //해당 프로세스의 모듈리스트를 루프로 돌려서 프로세스이름과 동일하면 
-    //true를 리턴한다.
-    if (Module32First(hModuleSnap, &me32))
-    {
-        do
-        {
-            printf("process name : %s\n", me32.szModule);
-            if (strcmp(me32.szModule, sProcessName) == 0)
-            {
-                sprintf(process, "%s 프로세스를 종료합니다.", me32.szModule);
-                MessageBox(NULL, process, "Error", MB_OK);
-                CloseHandle(hModuleSnap);
-                return true;
-            }
-            //if(me32.szModule == sProcessName)
-            //{ 
-            //    CloseHandle (hModuleSnap); 
-            //    return true;
-            //} 
-        } while (Module32Next(hModuleSnap, &me32));
-    }
-    CloseHandle(hModuleSnap);
-    return false;
-}
-
-
 int main() {
-    string processname[4] = { "Cheat Engine.exe","cheatengine-x86_64-SSE4-AVX2.exe", "x64dbg.exe", "x32dbg.exe" };
+    string processname[6] = { "cheatengine-x86_64.exe","cheatengine-i386.exe","cheatengine-x86_64-SSE4-AVX2.exe", "notepad.exe", "x32dbg.exe", "x64dbg.exe"};
     while (1) {
         for (int i = 0; i < sizeof(processname); i++) {
             char* process = const_cast<char*>(processname[i].c_str());
@@ -90,4 +52,3 @@ int main() {
         }
     }
 }
-
